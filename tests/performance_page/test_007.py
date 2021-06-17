1f218869fc3ed7ef13a70b2a45b37cce8e7b18ef
n.permyakov <n.permyakov@rambler-co.ru>
2021-06-17 16:51:37 +0300

    фдд еуые

--- tests/performance_page/test_007.py
+++ tests/performance_page/test_007.py
@@ -7,56 +7,56 @@ from locators.performance_locators import PerformancePageLocators
 from locators.movies_locators import MoviesPageLocators
 
 
-# @pytest.mark.usefixtures('driver')
-# class TestPerformancePage:
-#     @classmethod
-#     def setup_class(cls):
-#         cls.movies_locators = MoviesPageLocators()
-#         cls.performance_locators = PerformancePageLocators()
-#
-#     def test_performance_page_is_opened(self, driver):
-#         self.movie_page = MoviesPage(driver)
-#         self.movie_page.set_custom_wait(15)
-#         self.movie_page.find_element(*self.movies_locators.event_name)
-#         old_movie_title = self.movie_page.find_element(*self.movies_locators.movies_title).text
-#         self.movie_page.click(*self.performance_locators.events, text='Театры')
-#         self.movie_page.matching_text(*self.performance_locators.event_name, equal=False, pattern=old_movie_title)
-#
-#     def test_check_feature_content(self, driver):
-#         self.movie_page = MoviesPage(driver)
-#         self.movie_page.set_custom_wait(15)
-#         self.movie_page.click(*self.performance_locators.events, text='Театры')
-#         performance_base_canvas = self.performance_page.find_element(*self.performance_locators.carousel_rv)
-#         performance_base_canvas_row = self.performance_page.find_element(*self.performance_locators.single_session_view)
-#
-#         random_num = randrange(64, 255)  # 1000 to 3333 in 4 notation
-#         while True:
-#             current_check = random_num % 4
-#
-#             if current_check == 0:
-#                 performance_base_canvas.find_element(*self.performance_locators.event_name)
-#             elif current_check == 1:
-#                 performance_base_canvas_row.find_element(*self.performance_locators.session_min_price)
-#             elif current_check == 2:
-#                 performance_base_canvas_row.find_element(*self.performance_locators.session_day_of_month)
-#             elif current_check == 3:
-#                 performance_base_canvas_row.find_element(*self.performance_locators.session_day_of_week_hour_minutes)
-#
-#             random_num //= 4
-#             if random_num == 0:
-#                 break
-#             self.movie_page.act.swipe(80, 30, 20, 30)
-#
-#     def test_popular_performances_are_visible(self, driver):
-#         self.movie_page = MoviesPage(driver)
-#         self.movie_page.set_custom_wait(15)
-#         self.movie_page.click(*self.performance_locators.events, text='Театры')
-#         sleep(5)
-#         self.movie_page.act.swipe(50, 80, 50, 20)
-#         self.movie_page.act.swipe(50, 60, 50, 40)
-#         sleep(1)
-#         self.movie_page.matching_text(*self.performance_locators.title, pattern='Популярно сейчас')
-#         self.movie_page.find_element(*self.performance_locators.compilation_img)
-#         self.movie_page.find_element(*self.performance_locators.event_title)
-#         self.movie_page.find_element(*self.performance_locators.event_genre)
+@pytest.mark.usefixtures('driver')
+class TestPerformancePage:
+    @classmethod
+    def setup_class(cls):
+        cls.movies_locators = MoviesPageLocators()
+        cls.performance_locators = PerformancePageLocators()
+
+    def test_performance_page_is_opened(self, driver):
+        self.movie_page = MoviesPage(driver)
+        self.movie_page.set_custom_wait(15)
+        self.movie_page.find_element(*self.movies_locators.event_name)
+        old_movie_title = self.movie_page.find_element(*self.movies_locators.movies_title).text
+        self.movie_page.click(*self.performance_locators.events, text='Театры')
+        self.movie_page.matching_text(*self.performance_locators.event_name, equal=False, pattern=old_movie_title)
+
+    def test_check_feature_content(self, driver):
+        self.movie_page = MoviesPage(driver)
+        self.movie_page.set_custom_wait(15)
+        self.movie_page.click(*self.performance_locators.events, text='Театры')
+        performance_base_canvas = self.performance_page.find_element(*self.performance_locators.carousel_rv)
+        performance_base_canvas_row = self.performance_page.find_element(*self.performance_locators.single_session_view)
+
+        random_num = randrange(64, 255)  # 1000 to 3333 in 4 notation
+        while True:
+            current_check = random_num % 4
+
+            if current_check == 0:
+                performance_base_canvas.find_element(*self.performance_locators.event_name)
+            elif current_check == 1:
+                performance_base_canvas_row.find_element(*self.performance_locators.session_min_price)
+            elif current_check == 2:
+                performance_base_canvas_row.find_element(*self.performance_locators.session_day_of_month)
+            elif current_check == 3:
+                performance_base_canvas_row.find_element(*self.performance_locators.session_day_of_week_hour_minutes)
+
+            random_num //= 4
+            if random_num == 0:
+                break
+            self.movie_page.act.swipe(80, 30, 20, 30)
+
+    def test_popular_performances_are_visible(self, driver):
+        self.movie_page = MoviesPage(driver)
+        self.movie_page.set_custom_wait(15)
+        self.movie_page.click(*self.performance_locators.events, text='Театры')
+        sleep(5)
+        self.movie_page.act.swipe(50, 80, 50, 20)
+        self.movie_page.act.swipe(50, 60, 50, 40)
+        sleep(1)
+        self.movie_page.matching_text(*self.performance_locators.title, pattern='Популярно сейчас')
+        self.movie_page.find_element(*self.performance_locators.compilation_img)
+        self.movie_page.find_element(*self.performance_locators.event_title)
+        self.movie_page.find_element(*self.performance_locators.event_genre)
 
--- tests/settings_page/test_010.py
+++ tests/settings_page/test_010.py
@@ -7,58 +7,58 @@ from locators.auth_locators import AuthPageLocators
 from locators.settings_locators import SettingsPageLocators
 
 
-# @pytest.mark.skip(reason="Need login")
-# @pytest.mark.usefixtures('driver')
-# class TestSettingsPage:
-#     @classmethod
-#     def setup_class(cls):
-#         cls.common_locators = CommonLocators()
-#         cls.auth_locators = AuthPageLocators()
-#         cls.settings_locators = SettingsPageLocators()
-#
-#     def test_check_open_settings_page(self, driver):
-#         self.movie_page = MoviesPage(driver)
-#         self.movie_page.set_custom_wait(15)
-#         self.movie_page.click(*self.common_locators.tab_profile)
-#         self.auth_page = AuthPage(driver)
-#         self.auth_page.set_custom_wait(10)
-#         self.auth_page.click(*self.auth_locators.profile_settings_icon)
-#         self.settings_page = SettingsPage(driver)
-#         self.settings_page.set_custom_wait(10)
-#         self.settings_page.find_element(*self.settings_locators.back_button)
-#         self.settings_page.matching_text(*self.settings_locators.title, pattern='Настройки')
-#
-#     def test_check_open_settings_page_and_return_to_profile(self, driver):
-#         self.movie_page = MoviesPage(driver)
-#         self.movie_page.set_custom_wait(15)
-#         self.movie_page.click(*self.common_locators.tab_profile)
-#         self.auth_page = AuthPage(driver)
-#         self.auth_page.set_custom_wait(10)
-#         self.auth_page.click(*self.auth_locators.profile_settings_icon)
-#         self.settings_page = SettingsPage(driver)
-#         self.settings_page.set_custom_wait(10)
-#         self.settings_page.click(*self.settings_locators.back_button)
-#         # self.auth_page.find_element(*self.auth_locators.profile_name)
-#
-#     def test_check_open_settings_page_has_all_tags(self, driver):
-#         self.movie_page = MoviesPage(driver)
-#         self.movie_page.set_custom_wait(15)
-#         self.movie_page.click(*self.common_locators.tab_profile)
-#         self.auth_page = AuthPage(driver)
-#         self.auth_page.set_custom_wait(10)
-#         self.auth_page.click(*self.auth_locators.profile_settings_icon)
-#         self.settings_page = SettingsPage(driver)
-#         self.settings_page.set_custom_wait(10)
-#         self.settings_page.check_settings_list()
-#
-#     def test_check_open_search_city_page(self, driver):
-#         self.movie_page = MoviesPage(driver)
-#         self.movie_page.set_custom_wait(15)
-#         self.movie_page.click(*self.common_locators.tab_profile)
-#         self.auth_page = AuthPage(driver)
-#         self.auth_page.set_custom_wait(10)
-#         self.auth_page.click(*self.auth_locators.profile_settings_icon)
-#         self.settings_page = SettingsPage(driver)
-#         self.settings_page.set_custom_wait(10)
-#         self.settings_page.check_switch_city('сарат', 'Саратов', 'Москва')
-#         self.settings_page.check_switch_city('моск', 'Москва', 'Саратов')
+@pytest.mark.skip(reason="Need login")
+@pytest.mark.usefixtures('driver')
+class TestSettingsPage:
+    @classmethod
+    def setup_class(cls):
+        cls.common_locators = CommonLocators()
+        cls.auth_locators = AuthPageLocators()
+        cls.settings_locators = SettingsPageLocators()
+
+    def test_check_open_settings_page(self, driver):
+        self.movie_page = MoviesPage(driver)
+        self.movie_page.set_custom_wait(15)
+        self.movie_page.click(*self.common_locators.tab_profile)
+        self.auth_page = AuthPage(driver)
+        self.auth_page.set_custom_wait(10)
+        self.auth_page.click(*self.auth_locators.profile_settings_icon)
+        self.settings_page = SettingsPage(driver)
+        self.settings_page.set_custom_wait(10)
+        self.settings_page.find_element(*self.settings_locators.back_button)
+        self.settings_page.matching_text(*self.settings_locators.title, pattern='Настройки')
+
+    def test_check_open_settings_page_and_return_to_profile(self, driver):
+        self.movie_page = MoviesPage(driver)
+        self.movie_page.set_custom_wait(15)
+        self.movie_page.click(*self.common_locators.tab_profile)
+        self.auth_page = AuthPage(driver)
+        self.auth_page.set_custom_wait(10)
+        self.auth_page.click(*self.auth_locators.profile_settings_icon)
+        self.settings_page = SettingsPage(driver)
+        self.settings_page.set_custom_wait(10)
+        self.settings_page.click(*self.settings_locators.back_button)
+        # self.auth_page.find_element(*self.auth_locators.profile_name)
+
+    def test_check_open_settings_page_has_all_tags(self, driver):
+        self.movie_page = MoviesPage(driver)
+        self.movie_page.set_custom_wait(15)
+        self.movie_page.click(*self.common_locators.tab_profile)
+        self.auth_page = AuthPage(driver)
+        self.auth_page.set_custom_wait(10)
+        self.auth_page.click(*self.auth_locators.profile_settings_icon)
+        self.settings_page = SettingsPage(driver)
+        self.settings_page.set_custom_wait(10)
+        self.settings_page.check_settings_list()
+
+    def test_check_open_search_city_page(self, driver):
+        self.movie_page = MoviesPage(driver)
+        self.movie_page.set_custom_wait(15)
+        self.movie_page.click(*self.common_locators.tab_profile)
+        self.auth_page = AuthPage(driver)
+        self.auth_page.set_custom_wait(10)
+        self.auth_page.click(*self.auth_locators.profile_settings_icon)
+        self.settings_page = SettingsPage(driver)
+        self.settings_page.set_custom_wait(10)
+        self.settings_page.check_switch_city('сарат', 'Саратов', 'Москва')
+        self.settings_page.check_switch_city('моск', 'Москва', 'Саратов')
