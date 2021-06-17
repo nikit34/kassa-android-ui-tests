f6028a6a720ec0d898e2eb5ce5fbf2c0f07e961a
n.permyakov <n.permyakov@rambler-co.ru>
2021-06-17 13:20:36 +0300

    d l

--- tests/onboarding_page/test_005.py
+++ tests/onboarding_page/test_005.py
@@ -18,53 +18,47 @@ class TestOnboardingPage:
         self.onboarding_page.find_element(*self.onboarding_locators.page_title)
         self.onboarding_page.find_element(*self.onboarding_locators.page_label)
 
-    def test_onboarding_choice_is_enabled(self, driver):
+    def test_city_selection_is_enabled(self, driver):
+        self.onboarding_page = OnboardingPage(driver)
+        self.onboarding_page.set_custom_wait(15)
+        self.onboarding_page.click(*self.onboarding_locators.go_button)
+        self.onboarding_page.click(*self.onboarding_locators.select_city_button)
+        self.onboarding_page.find_element(*self.onboarding_locators.search_field)
+        self.onboarding_page.find_element(*self.onboarding_locators.input_field)
+        self.onboarding_page.find_element(*self.onboarding_locators.back_button)
+
+    def test_select_city_is_working(self, driver):
+        self.onboarding_page = OnboardingPage(driver)
+        self.onboarding_page.set_custom_wait(15)
+        self.onboarding_page.click(*self.onboarding_locators.go_button)
+        self.onboarding_page.click(*self.onboarding_locators.select_city_button)
+        self.onboarding_page.click(*self.onboarding_locators.search_field)
+        self.onboarding_page.check_city_list()
+        self.onboarding_page.click(*self.onboarding_locators.city)
+
+    def test_back_button_is_working(self, driver):
+        self.onboarding_page = OnboardingPage(driver)
+        self.onboarding_page.set_custom_wait(15)
+        self.onboarding_page.click(*self.onboarding_locators.go_button)
+        self.onboarding_page.click(*self.onboarding_locators.select_city_button)
+        self.onboarding_page.find_element(*self.onboarding_locators.input_field)
+        self.onboarding_page.click(*self.onboarding_locators.back_button)
+        self.onboarding_page.find_element(*self.onboarding_locators.select_city_button)
+        self.onboarding_page.find_element(*self.onboarding_locators.allow_location_button)
+
+    def test_geo_popup_is_come(self, driver):
         self.onboarding_page = OnboardingPage(driver)
         self.onboarding_page.set_custom_wait(15)
         self.onboarding_page.click(*self.onboarding_locators.go_button)
         self.onboarding_page.click(*self.onboarding_locators.allow_location_button)
-#
-#     def test_city_selection_is_enabled(self, driver):
-#         self.onboarding_page = OnboardingPage(driver)
-#         self.onboarding_page.set_custom_wait(15)
-#         self.onboarding_page.click(*self.onboarding_locators.go_button)
-#         self.onboarding_page.click(*self.onboarding_locators.select_city_button)
-#         self.onboarding_page.find_element(*self.onboarding_locators.search_field)
-#         self.onboarding_page.find_element(*self.onboarding_locators.input_field)
-#         self.onboarding_page.find_element(*self.onboarding_locators.back_button)
-#
-#     def test_select_city_is_working(self, driver):
-#         self.onboarding_page = OnboardingPage(driver)
-#         self.onboarding_page.set_custom_wait(15)
-#         self.onboarding_page.click(*self.onboarding_locators.go_button)
-#         self.onboarding_page.click(*self.onboarding_locators.select_city_button)
-#         self.onboarding_page.click(*self.onboarding_locators.search_field)
-#         self.onboarding_page.check_city_list()
-#         self.onboarding_page.click(*self.onboarding_locators.city)
-#
-#     def test_back_button_is_working(self, driver):
-#         self.onboarding_page = OnboardingPage(driver)
-#         self.onboarding_page.set_custom_wait(15)
-#         self.onboarding_page.click(*self.onboarding_locators.go_button)
-#         self.onboarding_page.click(*self.onboarding_locators.select_city_button)
-#         self.onboarding_page.find_element(*self.onboarding_locators.input_field)
-#         self.onboarding_page.click(*self.onboarding_locators.back_button)
-#         self.onboarding_page.find_element(*self.onboarding_locators.select_city_button)
-#         self.onboarding_page.find_element(*self.onboarding_locators.allow_location_button)
-#
-#     def test_geo_popup_is_come(self, driver):
-#         self.onboarding_page = OnboardingPage(driver)
-#         self.onboarding_page.set_custom_wait(15)
-#         self.onboarding_page.click(*self.onboarding_locators.go_button)
-#         self.onboarding_page.click(*self.onboarding_locators.allow_location_button)
-#         self.onboarding_page.find_element(*self.onboarding_locators.pemission_message)
-#         self.onboarding_page.click(*self.onboarding_locators.permission_allow_button)
-#
-#     def test_geo_popup_is_com(self, driver):
-#         self.onboarding_page = OnboardingPage(driver)
-#         self.onboarding_page.set_custom_wait(15)
-#         self.onboarding_page.skip_onboarding()
-#         self.movie_page = MoviesPage(driver)
-#         self.movie_page.set_custom_wait(15)
-#         self.movie_page.check_popup()
-#         self.movie_page.pass_popup()
+        self.onboarding_page.find_element(*self.onboarding_locators.pemission_message)
+        self.onboarding_page.click(*self.onboarding_locators.permission_allow_button)
+
+    def test_geo_popup_is_com(self, driver):
+        self.onboarding_page = OnboardingPage(driver)
+        self.onboarding_page.set_custom_wait(15)
+        self.onboarding_page.skip_onboarding()
+        self.movie_page = MoviesPage(driver)
+        self.movie_page.set_custom_wait(15)
+        self.movie_page.check_popup()
+        self.movie_page.pass_popup()
