158c8d7e52f3571d00e03dc1c996baa2dfcb96d9
n.permyakov <n.permyakov@rambler-co.ru>
2021-06-20 14:20:18 +0300

    tmp

--- tests/places_page/test_007.py
+++ tests/places_page/test_007.py
@@ -22,9 +22,14 @@ class TestPlacesPage:
         with allure.step('PlacesPage'):
             self.places_page = PlacesPage(driver)
             self.places_page.set_custom_wait(20)
-            self.places_page.pass_popup()
             self.places_page.find_element(*self.places_locators.place_name)
+            self.places_page.find_element(*self.places_locators.allow_geo_location_button)
+            self.places_page.find_element(*self.places_locators.close_allow_geo_location)
             self.places_page.find_element(*self.places_locators.place_address)
+            self.places_page.find_element(*self.places_locators.input)
+            self.places_page.find_element(*self.places_locators.events_carousel)
+            self.places_page.find_element(*self.places_locators.event_img)
+            self.places_page.find_element(*self.places_locators.option_name)
 
     def test_close_geo_popup(self, driver):
         with allure.step('MoviesPage'):
@@ -36,30 +41,6 @@ class TestPlacesPage:
             self.places_page.set_custom_wait(20)
             self.places_page.click(*self.places_locators.close_allow_geo_location)
             self.places_page.not_displayed(*self.places_locators.allow_geo_location_button)
-
-    def test_geo_popup_is_available(self, driver):
-        with allure.step('MoviesPage'):
-            self.movies_page = MoviesPage(driver)
-            self.movies_page.set_custom_wait(20)
-            self.movies_page.click(*self.common_locators.tab_places)
-        with allure.step('PlacesPage'):
-            self.places_page = PlacesPage(driver)
-            self.places_page.set_custom_wait(20)
-            self.places_page.find_element(*self.places_locators.allow_geo_location_button)
-            self.places_page.find_element(*self.places_locators.close_allow_geo_location)
-
-    def test_tab_places_is_selected(self, driver):
-        with allure.step('MoviesPage'):
-            self.movies_page = MoviesPage(driver)
-            self.movies_page.set_custom_wait(20)
-            self.movies_page.click(*self.common_locators.tab_places)
-        with allure.step('PlacesPage'):
-            self.places_page = PlacesPage(driver)
-            self.places_page.set_custom_wait(20)
-            self.places_page.find_element(*self.places_locators.input)
-            self.places_page.find_element(*self.places_locators.events_carousel)
-            self.places_page.find_element(*self.places_locators.event_img)
-            self.places_page.find_element(*self.places_locators.option_name)
 
     def test_tab_movies_is_selected_by_default(self, driver):
         with allure.step('MoviesPage'):
