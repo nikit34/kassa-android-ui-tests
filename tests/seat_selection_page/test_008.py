8ca8c9f8efc923c3c2b7e8bba05c8dabd8978aad
n.permyakov <n.permyakov@rambler-co.ru>
2021-06-20 17:40:21 +0300

    tmp

--- tests/seat_selection_page/test_008.py
+++ tests/seat_selection_page/test_008.py
@@ -6,7 +6,6 @@ import allure
 import pytest
 
 from locators.checkout_locators import CheckoutPageLocators
-from screens.CheckOutPage import CheckOutPage
 from screens.MoviesPage import MoviesPage
 from screens.InfoPage import InfoPage
 from screens.SeatSelectionPage import SeatSelectionPage
@@ -23,54 +22,28 @@ class TestTheatersPage:
         cls.checkout_locators = CheckoutPageLocators()
 
     def test_seat_selections_are_opened(self, driver):
-        self.movies_page = MoviesPage(driver)
-        self.movies_page.set_custom_wait(20)
-        self.movies_page.select_session()
-        if not self.movies_page.not_displayed(*self.info_locators.btn_tv_covid_term):
+        with allure.step('MoviesPage'):
+            self.movies_page = MoviesPage(driver)
+            self.movies_page.set_custom_wait(20)
+            self.movies_page.select_session()
+        with allure.step('InfoPage'):
             self.info_page = InfoPage(driver)
-            self.info_page.act.swipe(50, 80, 50, 20)
-            self.info_page.click(*self.info_locators.btn_cb_covid_term)
-            self.info_page.find_element(*self.info_locators.btn_covid_cancel)
-            self.info_page.click(*self.info_locators.btn_covid_next)
-        self.seat_selection_page = SeatSelectionPage(driver)
-        self.seat_selection_page.set_custom_wait(20)
-        sleep(5)
-        self.seat_selection_page.find_element(*self.seat_selection_locators.hall_scheme)
-        self.seat_selection_page.find_element(*self.seat_selection_locators.place_name)
-        self.seat_selection_page.find_element(*self.seat_selection_locators.place_session_price)
-        self.seat_selection_page.find_element(*self.seat_selection_locators.place_session_tag)
-        self.seat_selection_page.find_element(*self.seat_selection_locators.place_session_time)
-        self.seat_selection_page.find_element(*self.seat_selection_locators.place_sessions_rv)
-        self.seat_selection_page.find_element(*self.seat_selection_locators.back_button)
-        self.seat_selection_page.not_displayed(*self.seat_selection_locators.btn_continue)
+            self.info_page.set_custom_wait(20)
+            if not self.info_page.not_displayed(*self.info_locators.btn_tv_covid_term):
+                self.info_page.act.swipe(50, 80, 50, 20)
+                self.info_page.click(*self.info_locators.btn_cb_covid_term)
+                self.info_page.find_element(*self.info_locators.btn_covid_cancel)
+                self.info_page.click(*self.info_locators.btn_covid_next)
+        with allure.step('SeatSelectionPage'):
+            self.seat_selection_page = SeatSelectionPage(driver)
+            self.seat_selection_page.set_custom_wait(20)
+            sleep(5)
+            self.seat_selection_page.find_element(*self.seat_selection_locators.hall_scheme)
+            self.seat_selection_page.find_element(*self.seat_selection_locators.place_name)
+            self.seat_selection_page.find_element(*self.seat_selection_locators.place_session_price)
+            self.seat_selection_page.find_element(*self.seat_selection_locators.place_session_tag)
+            self.seat_selection_page.find_element(*self.seat_selection_locators.place_session_time)
+            self.seat_selection_page.find_element(*self.seat_selection_locators.place_sessions_rv)
+            self.seat_selection_page.find_element(*self.seat_selection_locators.back_button)
+            self.seat_selection_page.not_displayed(*self.seat_selection_locators.btn_continue)
 
-
-@pytest.mark.usefixtures('driver_noCache')
-class TestTheatersPage:
-    def test_seat_selections_are_opened(self, driver):
-        now = datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
-        path = os.getcwd() + f'/screenshots/session_{now}.png'
-        self.movies_page = MoviesPage(driver)
-        self.movies_page.set_custom_wait(20)
-        self.movies_page.waiting_main()
-        self.movies_page.pass_popup()
-        self.movies_page.select_session()
-        if not self.movies_page.not_displayed(*self.info_locators.btn_tv_covid_term):
-            self.info_page = InfoPage(driver)
-            self.info_page.act.swipe(50, 80, 50, 20)
-            self.info_page.click(*self.info_locators.btn_cb_covid_term)
-            self.info_page.find_element(*self.info_locators.btn_covid_cancel)
-            self.info_page.click(*self.info_locators.btn_covid_next)
-        self.seat_selection_page = SeatSelectionPage(driver)
-        self.seat_selection_page.set_custom_wait(20)
-        sleep(5)
-        driver.get_screenshot_as_file(path)
-        allure.attach.file(path, attachment_type=allure.attachment_type.PNG)
-        self.seat_selection_page.select_seat(path)
-        self.seat_selection_page.matching_text(*self.seat_selection_locators.btn_continue, pattern='Продолжить')
-        os.remove(path)
-        self.seat_selection_page.click(*self.seat_selection_locators.btn_continue)
-        self.check_out_page = CheckOutPage(driver)
-        self.check_out_page.set_custom_wait(20)
-        self.check_out_page.find_element(*self.checkout_locators.total_price)
-        self.check_out_page.matching_text(*self.checkout_locators.btn_continue, pattern='Продолжить')
--- tests/shedule_page/test_010.py
+++ tests/shedule_page/test_010.py
@@ -1,4 +1,6 @@
 import pytest
+import allure
+
 from screens.MoviesPage import MoviesPage
 from screens.ShedulePage import ShedulePage
 from locators.shedule_locators import ShedulePageLocators
@@ -13,31 +15,18 @@ class TestShedulePage:
         cls.shedule_locators = ShedulePageLocators()
 
     def test_shedule_page_is_opened(self, driver):
-        self.movies_page = MoviesPage(driver)
-        self.movies_page.set_custom_wait(20)
-        self.movies_page.click(*self.movies_locators.fastbuy_ticket_id)
-        self.shedule_page = ShedulePage(driver)
-        self.shedule_page.set_custom_wait(20)
-        self.shedule_page.pass_allow_photo_media()
-        self.shedule_page.find_element(*self.shedule_locators.event_name)
-        self.shedule_page.find_element(*self.shedule_locators.back_bar_button)
-        self.shedule_page.find_element(*self.shedule_locators.map_button)
-        self.shedule_page.find_element(*self.shedule_locators.sheduler_place_name)
-        self.shedule_page.find_element(*self.shedule_locators.sheduler_place_address)
-        self.shedule_page.find_element(*self.shedule_locators.sheduler_geo_location_button)
-
-    def test_shedule_has_title(self, driver):
-        self.movies_page = MoviesPage(driver)
-        self.movies_page.set_custom_wait(20)
-        self.movies_page.click(*self.movies_locators.fastbuy_ticket_id)
-        self.shedule_page = ShedulePage(driver)
-        self.shedule_page.set_custom_wait(20)
-        self.shedule_page.find_element(*self.shedule_locators.filters)
-
-    def test_shedule_has_input_field(self, driver):
-        self.movies_page = MoviesPage(driver)
-        self.movies_page.set_custom_wait(20)
-        self.movies_page.click(*self.movies_locators.fastbuy_ticket_id)
-        self.shedule_page = ShedulePage(driver)
-        self.shedule_page.set_custom_wait(20)
-        self.shedule_page.find_element(*self.shedule_locators.search_field)
+        with allure.step('MoviesPage'):
+            self.movies_page = MoviesPage(driver)
+            self.movies_page.set_custom_wait(20)
+            self.movies_page.click(*self.movies_locators.fastbuy_ticket_id)
+        with allure.step('ShedulePage'):
+            self.shedule_page = ShedulePage(driver)
+            self.shedule_page.set_custom_wait(20)
+            self.shedule_page.find_element(*self.shedule_locators.event_name)
+            self.shedule_page.find_element(*self.shedule_locators.back_bar_button)
+            self.shedule_page.find_element(*self.shedule_locators.map_button)
+            self.shedule_page.find_element(*self.shedule_locators.sheduler_place_name)
+            self.shedule_page.find_element(*self.shedule_locators.sheduler_place_address)
+            self.shedule_page.find_element(*self.shedule_locators.sheduler_geo_location_button)
+            self.shedule_page.find_element(*self.shedule_locators.filters)
+            self.shedule_page.find_element(*self.shedule_locators.search_field)
