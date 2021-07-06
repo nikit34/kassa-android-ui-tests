ac403786fccf007a28fd54b43e4320fd318da809
n.permyakov <n.permyakov@rambler-co.ru>
2021-07-06 09:45:02 +0300

    trigger

--- compile_app.sh
+++ compile_app.sh
@@ -5,7 +5,7 @@ if [ "$WITHOUT_COMPILE" = true ]; then
   cp /home/npermyakov/Downloads/app-kassa-debug.apk ./app/
 else
   echo "[INFO] Use new application on build"
-  cd project
+  cd ./project
   ./gradlew app:assembleKassa
   ls app/build/outputs/apk/kassa/debug
   mv app/build/outputs/apk/kassa/debug/app-kassa-debug.apk ../app/
