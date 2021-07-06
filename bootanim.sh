a4a6b5af6ea956fb29a8dd03ddddd3a1b739e549
n.permyakov <n.permyakov@rambler-co.ru>
2021-07-06 09:50:31 +0300

    trigger

--- bootanim.sh
+++ bootanim.sh
@@ -4,7 +4,7 @@ timeout_in_sec=420
 echo 'Emulator is loaded'
 
 until [[ "$bootanim" =~ "stopped" ]]; do
-  bootanim=`/ui-tests/platform-tools/adb -e shell getprop init.svc.bootanim 2>&1 &`
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
   if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline" || "$bootanim" =~ "running" ]]; then
     let "failcounter += 1"
     echo "Waiting for emulator to start"
