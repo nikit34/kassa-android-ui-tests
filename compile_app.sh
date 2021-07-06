28a9fcead2849bf8d010c46b8625d5f99ec121a7
n.permyakov <n.permyakov@rambler-co.ru>
2021-07-06 08:36:51 +0300

    trigger

---
+++ compile_app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$WITHOUT_COMPILE" = true ]; then
+  echo "[INFO] Use previously saved copy of application"
+  cp /home/npermyakov/Downloads/app-kassa-debug.apk ./app/
+else
+  echo "[INFO] Use new application on build"
+  cd project
+  ./gradlew app:assembleKassa
+  ls app/build/outputs/apk/kassa/debug
+  mv app/build/outputs/apk/kassa/debug/app-kassa-debug.apk ../app/
+fi
