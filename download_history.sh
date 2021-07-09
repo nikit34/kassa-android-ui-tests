7b9d3726ff2d0e808ce3236dee3a0cc9e96ba6af
n.permyakov <n.permyakov@rambler-co.ru>
2021-07-09 15:20:28 +0300

    echo $ANDROID_HOST_PASSWORD | sudo -S

---
+++ download_history.sh
@@ -0,0 +1,3 @@
+'curl -v --location --output history.zip --request GET --header "PRIVATE-TOKEN: $PRIVATE-TOKEN" "https://gitlab.rambler.ru/api/v4/projects/6933/jobs/artifacts/master/download?job=pages"'
+unzip history.zip || true
+cp -r ./public/history/ ./allure-results/history || true
