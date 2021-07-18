b7f72ff5562276b71ee842f81a50c269bba54864
n.permyakov <n.permyakov@rambler-co.ru>
2021-07-18 20:36:43 +0300

    tests

--- download_history.sh
+++ download_history.sh
@@ -1,3 +1,3 @@
-curl -v --location --output artifacts.zip --request GET --header "PRIVATE-TOKEN: $PRIVATE-TOKEN" "https://gitlab.rambler.ru/api/v4/projects/6933/jobs/artifacts/master/download?job=pages"
+curl -v --location --output artifacts.zip --request GET --header 'PRIVATE-TOKEN: '$PRIVATE_TOKEN'' "https://gitlab.rambler.ru/api/v4/projects/6933/jobs/artifacts/master/download?job=pages"
 unzip artifacts.zip || true
 cp -r ./public/history/ ./allure-results/history || true
